13fbbe0a5dea9712a4ddec4c0dd027953c9f01b1
DevinAnnunzio <annunzio.da@gmail.com>
2023-11-10 17:26:16 +0900

    Feat: update impl methods to use spring data JPA

--- src/main/java/com/annunzio/employeecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/annunzio/employeecrud/service/EmployeeServiceImpl.java
@@ -1,39 +1,54 @@
 package com.annunzio.employeecrud.service;
 
 import com.annunzio.employeecrud.dao.EmployeeDAO;
+import com.annunzio.employeecrud.dao.EmployeeRepository;
 import com.annunzio.employeecrud.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO empDAO){
-        this.employeeDAO = empDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
+        employeeRepository = theEmployeeRepository;
     }
     @Override
     public List<Employee> findAllEmployees() {
-        return employeeDAO.findAllEmployees();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int empId) {
-        return employeeDAO.findById(empId);
+        Optional<Employee> search = employeeRepository.findById(empId);
+        Employee possibleEmp = null;
+
+        if(search.isPresent()){
+            System.out.println("HERRRRRRRRRE");
+            System.out.println(search);
+            System.out.println(search.get());
+            possibleEmp = search.get();
+            return possibleEmp;
+        } else {
+            throw new RuntimeException("Could not find the employee requested with an id of {" + empId + "}.");
+        }
     }
 
-    @Transactional
+    //JPA provide @Transactional
+//    @Transactional
     @Override
     public Employee save(Employee theEmp) {
-        return employeeDAO.save(theEmp);
+        return employeeRepository.save(theEmp);
     }
 
-    @Transactional
+//    @Transactional
     @Override
     public void deleteEmployeeById(int empId) {
-        employeeDAO.deleteEmployeeById(empId);
+        employeeRepository.deleteById(empId);
     }
 }
