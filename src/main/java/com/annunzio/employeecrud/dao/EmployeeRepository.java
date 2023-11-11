dc7c7579c70cf0b88cb7c74ab1e2619c4ae1e701
DevinAnnunzio <annunzio.da@gmail.com>
2023-11-11 17:18:41 +0900

    Build: Added spring security dependency

--- src/main/java/com/annunzio/employeecrud/dao/EmployeeRepository.java
+++ src/main/java/com/annunzio/employeecrud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.annunzio.employeecrud.dao;
 
 import com.annunzio.employeecrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="workers")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //No need for implementation class with Spring Data JPA
 
