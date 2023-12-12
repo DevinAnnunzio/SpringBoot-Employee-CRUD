90f7171063ad8560d5daf56f30178351d8fecf4c
DevinAnnunzio <annunzio.da@gmail.com>
2023-12-12 17:03:08 +0900

    Feat: Security/Restrictions based on roles

--- src/main/java/com/annunzio/employeecrud/security/DemoSecurityConfig.java
+++ src/main/java/com/annunzio/employeecrud/security/DemoSecurityConfig.java
@@ -3,9 +3,13 @@ package com.annunzio.employeecrud.security;
 import com.annunzio.employeecrud.entity.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -34,5 +38,24 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, mike, bob);
 
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/company/workers").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/company/workers/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/company/workers").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/company/workers").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/company/workers/**").hasRole("ADMIN")
+                );
+        //HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        //Disable CSRF since it is stateless API
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
 
 }
